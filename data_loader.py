a352149b1ad5834fdae44d68c63cdeda82d4d077
BezLiz <lizabezzubova@icloud.com>
2025-10-14 04:23:05 +0300

    Update data_loader.py

--- data_loader.py
+++ data_loader.py
@@ -15,6 +15,41 @@ try:
 except Exception as e:
     print("Ошибка при чтении файла:", e)
 
+# ПРИВЕДЕНИЕ ТИПОВ ДАННЫХ
 
+df = pd.DataFrame(raw_data)
+
+# Разделяем значения pubmed_id, так как они неправильно приведены в датаете
+df["Pubmed_ID"] = df["Pubmed_ID"].astype(str).str.split("##")
+df = df.explode("Pubmed_ID", ignore_index=True)
+
+print("\nТипы ДО приведения:")
+print(df.dtypes)
+
+# Числовые данные
+numeric_cols = ["Sequence_Length", "Pubmed_ID"]
+for col in numeric_cols:
+    if col in df.columns:
+        df[col] = pd.to_numeric(df[col], errors="coerce").astype("Int64")
+# Категориальные данные
+categorical_cols = ["Structure", "Linear/Cyclic/Branched", "Stereochemistry"]
+for col in categorical_cols:
+    if col in df.columns:
+        df[col] = df[col].astype("category")
+# Текстовые данные
+text_cols = [col for col in df.columns if col not in numeric_cols + categorical_cols]
+for col in text_cols:
+    df[col] = df[col].astype(str)
+
+print("\nТипы ПОСЛЕ приведения:")
+print(df.dtypes)
+
+print("\nПропуски после приведения:")
+print(df.isnull().sum())
+
+# Сохранение данных 
+out_path = "Data_clean.parquet"
+df.to_parquet(out_path, index=False)
+print(f"\n[OK] Данные успешно приведены к нужным типам и сохранены в {out_path}")
 
 
